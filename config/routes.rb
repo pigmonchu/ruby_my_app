ab11e3cb4c768b4d7c2709c10a798952bfd05402
Mon Maldonado <ramon@digestivethinking.com>
2018-01-23 20:26:13 +0100

    Creación simple de katas

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'katas#index' #va a vistas -> katas -> index
 
   get '/kata/:id' => 'katas#show', as: :kata #el le añade _path
+  get '/new/kata' => 'katas#new', as: :new_kata
+  post '/create/kata' => 'katas#create', as: :create_kata
+
 end
